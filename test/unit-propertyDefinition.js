73d0a5a6238f9c9e72dab0df12d697a5398da481
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-01-30 15:54:47 +0100

    refactored, linted

--- test/unit-propertyDefinition.js
+++ test/unit-propertyDefinition.js
@@ -1,7 +1,31 @@
+"use strict";
 var assert = require("assert"),
     Definition = require("../lib/PropertyDefinition");
 
 var CustomClass = function(){};
+
+function lessThan10(value) {
+    return value < 10;
+}
+function allLessThan10(arr){
+    return arr.every(lessThan10);
+}
+function allFamily(arr){
+    return arr.every(function(member){
+        return /^(dad|sister|brother|mother)$/.test(member);
+    });
+}
+function allFamilyElse(arr){
+    var self = this;
+    return arr.every(function(member){
+        if(/^(dad|sister|brother|mother)$/.test(member)){
+            return true;
+        } else {
+            self.addValidationMessage("this one is invalid: " + member);
+            return false;
+        }
+    });
+}
 
 function factory(name){
     var definitions = {
@@ -14,32 +38,18 @@ function factory(name){
         array:  { name: "one", type: Array }),
         custom: { name: "one", type: CustomClass }),
         date:   { name: "one", type: Date }),
-        
-        { name: "one", type: "number", default: 4, valueTest: smallNumber });
-        { name: "one", type: Array, default: [0,4,6], valueTest: smallArray });
-        { name: "one", type: "number", required: true, default: 1 });
-        { name: "one", type: Array, required: true, valueTest: function(files){
-        { name: "one",  type: Array,  valueTest: validArray,  valueFailMsg: "every value must be over 10" });
-        { name: "one", type: RegExp, value: "\\w{4}" });
-        { name: "one", type: "string", value: "ok" });
-        { name: "one", type: "number", value: "not ok" });
-        { name: "one", type: RegExp, value: /ok/ });
-        { name: "one", type: RegExp, typeFail: "pass a regex", value: "ok" });
-        { name: "one", type: RegExp, typeFail: "pass a regex", value: "+++" });
-        { name: "relative", type: "string", value: "dog", valueTest: /^(dad|sister|brother|mother)$/, valueFail: "invalid relative" });
-        { name: "family", type: Array, value: ["dad", "sister", "dog"], valueTest: /(dad|sister|brother|mother)/ });
-        { name: "family", type: Array, value: ["dad", "sister", "dog"], valueTest: function(family){ return family.every(function(member){ return /^(dad|sister|brother|mother)$/.test(member); }); }, valueFail: "every member must be valid" });
-        {name: "one", type: "number", valueTest: validTest, valueFailMsg: "must supply a value over 10" });    };
+        regex:  { name: "one", type: RegExp })
+    };
     return new Definition(definitions[name]);
 }
 
 describe("PropertyDefinition", function(){
     describe("properties: ", function(){
         it("access to `this.config` in a `valid` function must fail if config is not set");
-        
+
         it("should be ok to have an option with no defined type");
         it("type: [Array, Function] should allow a type of either");
-        
+
         describe(".valid", function(){
             it("valid if no type specified", function(){
                 var def = factory("name");
@@ -60,7 +70,7 @@ describe("PropertyDefinition", function(){
                 assert.strictEqual(def.valid, false);
                 def.value = [];
                 assert.strictEqual(def.valid, false);
-    
+
                 def = factory("number");
                 def.value = 123;
                 assert.strictEqual(def.valid, true);
@@ -108,7 +118,7 @@ describe("PropertyDefinition", function(){
                 assert.strictEqual(def.valid, false);
                 def.value = [];
                 assert.strictEqual(def.valid, false);
-    
+
                 def = factory("object");
                 def.value = 123;
                 assert.strictEqual(def.valid, false);
@@ -160,7 +170,7 @@ describe("PropertyDefinition", function(){
                 def.value = new Date();
                 assert.strictEqual(def.valid, true);
             });
-    
+
             it("valid with .required", function(){
                 /*
                 required means 'value should be truthy'
@@ -182,13 +192,13 @@ describe("PropertyDefinition", function(){
                 assert.strictEqual(def.valid, false);
                 def.value = 1;
                 assert.strictEqual(def.valid, true);
-            
+
                 def = factory("array");
                 assert.strictEqual(def.valid, true);
                 def.required = true;
                 assert.strictEqual(def.valid, false);
             });
-            
+
             it("valid with RegExp .valueTest", function(){
                 /*
                 any definition with a valueTest should always be tested. Required is implied.
@@ -203,7 +213,18 @@ describe("PropertyDefinition", function(){
                 def.value = "test";
                 assert.strictEqual(def.valid, true);
                 def.valueTest = /tast/;
-                assert.strictEqual(def.valid, false);            
+                assert.strictEqual(def.valid, false);
+
+                def.valueTest = /^(dad|sister|brother|mother)$/
+                def.value = "dog"
+                assert.strictEqual(def.valid, false);
+                def.value = "dad";
+                assert.strictEqual(def.valid, true);
+
+                def.type = Array;
+                def.valueTest = /(dad|sister|brother|mother)/
+                def.value = ["dad", "sister", "dog"];
+                assert.strictEqual(def.valid, true);
             });
 
             it("valid with primitive .valueTest", function(){
@@ -225,216 +246,133 @@ describe("PropertyDefinition", function(){
                 def.valueTest = "5";
                 assert.strictEqual(def.valid, false);
             });
-    
+
             it("valid with function .validTest", function(){
-                function smallNumber(value) {
-                    return value < 10;
-                }
-                var def = new Definition({ name: "one", type: "number", default: 4, valueTest: smallNumber });
+                var def = factory("number");
+                def.valueTest = lessThan10;
+                def.value = 4;
                 assert.strictEqual(def.valid, true);
+
                 def.value = 11;
                 assert.strictEqual(def.valid, false);
 
-                function smallArray(a){
-                    return a.length < 10;
-                }
-                def = new Definition({ name: "one", type: Array, default: [0,4,6], valueTest: smallArray });
+                def = factory("array");
+                def.valueTest = allLessThan10;
+                def.value = [0,4,6];
                 assert.strictEqual(def.valid, true);
-                def.value = [1,2,3,4,5,6,7,5,4,3,2,1,0];
+
+                def.value = [0,4,16];
+                assert.strictEqual(def.valid, false);
+
+                def.valueTest = allFamily;
+                def.value = ["dad", "sister", "dog"];
                 assert.strictEqual(def.valid, false);
             });
-    
+
             it("`valid` should accept and test an array of functions");
 
-            it("`valid` should return false if option `required` with no value set", function(){
-                var def = new Definition({ name: "one", type: "number", required: true });
-                assert.strictEqual(def.valid, false);
-        
-                def = new Definition({ name: "one", type: "number", required: true, default: 1 });
-                assert.strictEqual(def.valid, true);
-            });
-            
             describe("bad usage", function(){
-                it("`valid` should return `false` if `valueTest` function threw", function(){
-                    var def = new Definition({ name: "one", type: Array, required: true, valueTest: function(files){
+                it("valid with function .validTest - function throws", function(){
+                    var def = factory("array");
+                    def.valueTest = function(){
                         throw new Error("error");
-                    }});
-                
+                    });
                     def.value = ["test"];
                     assert.strictEqual(def.valid, false);
                 });
             });
         });
-        
+
         describe(".validationMessages", function(){
-            it("custom invalid msg with type `Array`", function(){
-                function validArray(values){
-                    return values.every(function(val){
-                        return val > 10;
-                    });
-                }
-                var def = new Definition({ name: "one",  type: Array,  valueTest: validArray,  valueFailMsg: "every value must be over 10" });
-            
-                def.value = [1];
-                assert.deepEqual(def.validationMessages, ["every value must be over 10"]);
-
-                def.value = [11];
-                assert.deepEqual(def.validationMessages, []);
-            });
-        });
-        
-        describe(".value", function(){
-            it("a number string should typecast to a Number type automatically", function(){
-               var def = new Definition({ name: "one", type: "number" }) ;
-           
-               def.value = "3";
-               assert.strictEqual(def.value, 3);
-               assert.strictEqual(def.valid, true);
-
-               def.value = "0";
-               assert.strictEqual(def.value, 0);
-               assert.strictEqual(def.valid, true);
-
-               def.value = "-1";
-               assert.strictEqual(def.value, -1);
-               assert.strictEqual(def.valid, true);
-
-               def.value = -1.5345;
-               assert.strictEqual(def.value, -1.5345);
-               assert.strictEqual(def.valid, true);
-
-               def.value = "-1.5345";
-               assert.strictEqual(def.value, -1.5345);
-               assert.strictEqual(def.valid, true);
-
-               def.value = "a";
-               assert.strictEqual(def.value, "a");
-               assert.strictEqual(def.valid, false);
-
-               def.value = "";
-               assert.strictEqual(def.value, "");
-               assert.strictEqual(def.valid, false);
-
-               def.value = true;
-               assert.strictEqual(def.value, true);
-               assert.strictEqual(def.valid, false);
-
-               def.value = function(){};
-               assert.strictEqual(def.valid, false);
-
-               def.value = null;
-               assert.strictEqual(def.value, null);
-               assert.strictEqual(def.valid, false);
-
-               def.value = undefined;
-               assert.strictEqual(def.value, undefined);
-               assert.strictEqual(def.valid, true); // if an option is not required an undefined value is ok
-            });
-            
-            it("a regex string should typecast to a RegExp", function(){
-                var def = new Definition({ name: "one", type: RegExp, value: "\\w{4}" });
-                assert.ok(def.value instanceof RegExp, def.value);
-                assert.deepEqual(def.value, /\w{4}/);
-            });
-        });
-        
-        describe("validation", function(){
-            it("type validation summary", function(){
-                var def = new Definition({ name: "one", type: "string", value: "ok" });
-                assert.strictEqual(def.valid, true);
-                assert.strictEqual(def.validationMessages.length, 0);
-                
-                def = new Definition({ name: "one", type: "number", value: "not ok" });
-                assert.strictEqual(def.valid, false);
-                assert.strictEqual(def.validationMessages.length, 1);
-
-                def = new Definition({ name: "one", type: RegExp, value: /ok/ });
-                assert.strictEqual(def.valid, true);
-                assert.strictEqual(def.validationMessages.length, 0);
-                
-                // "ok" parses by RegExp
-                def = new Definition({ name: "one", type: RegExp, typeFail: "pass a regex", value: "ok" });
-                assert.strictEqual(def.valid, true);
+            it("with no fail message", function(){
+                var def = factory("string");
+                def.value = "ok";
                 assert.strictEqual(def.validationMessages.length, 0);
 
-                // "+++" does not parse by RegExp
-                def = new Definition({ name: "one", type: RegExp, typeFail: "pass a regex", value: "+++" });
-                assert.strictEqual(def.valid, false);
+                def.type = "number";
                 assert.strictEqual(def.validationMessages.length, 1);
-            });
-            
-            it("value validation summary", function(){
-                var def = new Definition({ name: "relative", type: "string", value: "dog", valueTest: /^(dad|sister|brother|mother)$/, valueFail: "invalid relative" });
-                assert.strictEqual(def.valid, false);
-                assert.strictEqual(def.validationMessages.length, 1);
-                def.value = "dad";
-                assert.strictEqual(def.valid, true);
+
+                def.type = RegExp;
                 assert.strictEqual(def.validationMessages.length, 0);
+            });
 
-                def = new Definition({ name: "family", type: Array, value: ["dad", "sister", "dog"], valueTest: /(dad|sister|brother|mother)/ });
-                assert.strictEqual(def.valid, true);
+            it("with .valueFailMsg", function(){
+                it("with type Array", function(){
+                    var def = factory("array");
+                    def.valueTest = allLessThan10;
+                    def.valueFailMsg = "every value must be less than 10";
 
-                def = new Definition({ name: "family", type: Array, value: ["dad", "sister", "dog"], valueTest: function(family){ return family.every(function(member){ return /^(dad|sister|brother|mother)$/.test(member); }); }, valueFail: "every member must be valid" });
-                assert.strictEqual(def.valid, false);
-                assert.strictEqual(def.validationMessages.length, 1);
+                    def.value = [1];
+                    assert.deepEqual(def.validationMessages, ["every value must be less than 10"]);
 
-                def = new Definition({
-                    name: "family", 
-                    type: Array, 
-                    value: ["dad", "sister", "dog"], 
-                    valueTest: function(family){
-                        var self = this;
-                        return family.every(function(member){
-                            if (/^(dad|sister|brother|mother)$/.test(member)){
-                                return true;
-                            } else {
-                                self.addValidationMessage("this one is invalid: " + member);
-                                return false;
-                            }
-                        });
-                    },
-                    valueFail: "every member must be valid"
+                    def.value = [11];
+                    assert.deepEqual(def.validationMessages, []);
                 });
+            });
+
+            it("with .typeFailMsg", function(){
+                def = factory("regex");
+                def.typeFailMsg = "pass a regex";
+                def.value = "ok";
+                assert.strictEqual(def.validationMessages.length, 0);
+
+                def.value = "+++";
+                assert.strictEqual(def.validationMessages.length, 1);
+                assert.strictEqual(def.validationMessages[0], "pass a regex");
+            });
+
+            it("raised from .validTest function", function(){
+                var def = factory("array");
+                def.value = ["dad", "sister", "dog"];
+                def.valueTest = allFamilyElse,
+                def.valueFailMsg = "every member must be valid";
                 assert.strictEqual(def.valid, false);
                 assert.strictEqual(def.validationMessages.length, 2);
             });
-            
-            it("custom invalid msg with type `number`", function(){
-                function validTest(value){
-                    return value > 10;
-                }
-                var def = new Definition({name: "one", type: "number", valueTest: validTest, valueFailMsg: "must supply a value over 10" });
-            
-                def.value = 1;
-                assert.deepEqual(def.validationMessages, ["must supply a value over 10"]);
+        })
 
-                def.value = 11;
-                assert.deepEqual(def.validationMessages, []);
+        describe(".value", function(){
+            it("typecasting with number", function(){
+               var def = factory("number");
+
+               def.value = "3";
+               assert.strictEqual(def.value, 3);
+               def.value = "0";
+               assert.strictEqual(def.value, 0);
+               def.value = "-1";
+               assert.strictEqual(def.value, -1);
+               def.value = -1.5345;
+               assert.strictEqual(def.value, -1.5345);
+               def.value = "-1.5345";
+               assert.strictEqual(def.value, -1.5345);
+               def.value = "a";
+               assert.strictEqual(def.value, "a");
+               def.value = "";
+               assert.strictEqual(def.value, "");
+               def.value = true;
+               assert.strictEqual(def.value, true);
+               def.value = CustomClass;
+               assert.strictEqual(def.value, CustomClass);
+               def.value = null;
+               assert.strictEqual(def.value, null);
+               def.value = undefined;
+               assert.strictEqual(def.value, undefined);
             });
 
-            it("inserting error messages from a `valid` function", function(){
-                function validArray(values){
-                    var valid = true, self = this;
-                    values.forEach(function(val){
-                        if (val < 10){
-                            valid = false; 
-                            self.addValidationMessage("less than 10: " + val);
-                        }
-                    });
-                    return valid;
-                }
-                var def = new Definition({
-                    name: "one", 
-                    type: Array, 
-                    valueTest: validArray, 
-                    valueFailMsg: "every value must be over 10" 
-                });
-            
-                def.value = [1];
-                assert.deepEqual(def.validationMessages, ["less than 10: 1", "every value must be over 10"]);
+            it("typecasting with RegExp", function(){
+                var def = factory("regex");
+                def.value = "\\w{4}";
+                assert.ok(def.value instanceof RegExp, def.value);
+                assert.deepEqual(def.value, /\w{4}/);
 
-                def.value = [11];
-                assert.deepEqual(def.validationMessages, []);
+                def.value = "ok";
+                assert.ok(def.value instanceof RegExp);
+                assert.deepEqual(def.value, /ok/);
+
+                def.value = "+++";
+                assert.ok(!(def.value instanceof RegExp));
+                assert.strictEqual(def.value, "+++");
+
             });
         });
     });
