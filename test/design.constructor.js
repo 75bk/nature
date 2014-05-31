1581c3596ee4fdaaa0022ab5ae105e4c4a8deee6
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-31 19:06:17 +0100

    failing tests

--- test/design.constructor.js
+++ test/design.constructor.js
@@ -8,7 +8,41 @@ var attributes = [
 ];
 
 test("constructor returns design instance", function(t){
-    var design = Design(attributes);
+    var design = Design();
     t.ok(design instanceof Design);
+    t.end();
+});
+
+test("constructor(attributes)", function(t){
+    var design = Design(attributes);
+    t.equal(design._attributes, attributes);
+    t.end();
+});
+
+test("constructor(attributes) grouped", function(t){
+    var attributes = [
+        {
+            groups: "test1",
+            attributes: [
+                { name: "verbose", type: Boolean },
+                { name: "dry", type: Boolean }
+            ]
+        },
+        {
+            groups: "test2",
+            attributes: [
+                { name: "three", type: Boolean },
+                { name: "four", type: Boolean }
+            ]
+        }
+    ];
+
+    var design = Design(attributes);
+    t.deepEqual(design._attributes, [
+        { name: "verbose", type: Boolean, groups: "test1" },
+        { name: "dry", type: Boolean, groups: "test1" },
+        { name: "three", type: Boolean, groups: "test2" },
+        { name: "four", type: Boolean, groups: "test2" }
+    ]);
     t.end();
 });
