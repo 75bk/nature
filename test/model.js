1e2d084f80961d375848f1a5ea68ecba4bf1656d
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-31 14:22:11 +0100

    more passing tests

--- test/model.js
+++ test/model.js
@@ -17,3 +17,46 @@ test("Model has correct values", function(t){
     t.equal(model.colours, undefined);
     t.end();
 });
+
+test("Model has correct values", function(t){
+    var design = Design(attributes);
+    var model = design.create();
+
+    model.verbose = null;
+    model.colour = null;
+    model.number = null;
+    model.colours = null;
+    
+    t.equal(model.verbose, null);
+    t.equal(model.colour, null);
+    t.equal(model.number, null);
+    t.equal(model.colours, null);
+    t.end();
+});
+
+test("boolean values", function(t){
+    var design = Design(attributes);
+    var model = design.create();
+
+    model.verbose = "verbose";
+    t.equal(model.verbose, true);
+    model.verbose = "";
+    t.equal(model.verbose, false);
+    model.verbose = true;
+    t.equal(model.verbose, true);
+    model.verbose = 0;
+    t.equal(model.verbose, false);
+    t.end();
+});
+
+test("array values", function(t){
+    var design = Design(attributes);
+    var model = design.create();
+
+    model.colours = [ "red", "green" ];
+    t.deepEqual(model.colours, [ "red", "green" ]);
+    model.colours = "red";
+    t.deepEqual(model.colours, [ "red" ]);
+    t.end();
+});
+
