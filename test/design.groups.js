24a70ff3f39c9a6987f9532585b826e3e4482888
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-01 13:00:50 +0100

    grouping test

--- test/design.groups.js
+++ test/design.groups.js
@@ -24,3 +24,26 @@ test("design.where(model, { groups: 'blah' })", function(t){
     t.deepEqual(design.groups(), [ "test1", "test2" ]);
     t.end();
 });
+
+test("design.where(model, { groups: 'blah' })", function(t){
+    var attributes = [
+        {
+            groups: ["test1", "all"],
+            attributes: [
+                { name: "verbose", type: Boolean },
+                { name: "colour", type: String }
+            ]
+        },
+        {
+            groups: ["test2", "all"],
+            attributes: [
+                { name: "three", type: Boolean },
+                { name: "four", type: Boolean }
+            ]
+        }
+    ];
+
+    var design = Design(attributes);
+    t.deepEqual(design.groups(), [ "test1", "all", "test2" ]);
+    t.end();
+});
